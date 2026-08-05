9c4b180975ae2677fd8350006ea41ad3b4d00997
Butawantemi <japhetbuta@gmail.com>
2026-08-05 10:26:33 +0300

    courseinfo

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,65 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.part1} {props.exercises1}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {}
+      {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content
+        part1={course.parts[0].name}
+        exercises1={course.parts[0].exercises}
+      />
+      <Content
+        part1={course.parts[1].name}
+        exercises1={course.parts[1].exercises}
+      />
+      <Content
+        part1={course.parts[2].name}
+        exercises1={course.parts[2].exercises}
+      />
+      <Total
+        exercises1={course.parts[0].exercises}
+        exercises2={course.parts[0].exercises}
+        exercises3={course.parts[0].exercises}
+      />
+    </div>
+  );
+};
+
+export default App;
