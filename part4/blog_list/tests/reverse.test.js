9df5c83847ab92a4e2a670c5bd32999d64b99056
Butawantemi <japhetbuta@gmail.com>
2026-09-13 13:12:57 +0300

    blog_list  test  blog  with most likes and author with many likes

--- part4/blog_list/tests/reverse.test.js
+++ part4/blog_list/tests/reverse.test.js
@@ -12,6 +12,59 @@ test('dummy returns one', () => {
   assert.strictEqual(result, 1)
 })
 
+describe('blog with most likes', () => {
+  const listBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0
+    }
+
+  ]
+
+  test('Blog with most likes', () => {
+    const result = listHelper.mostLikes(listBlogs)
+    console.log(result)
+    assert.deepStrictEqual(result, { author: listBlogs[1].author, likes: listBlogs[1].likes })
+  })
+})
+
+
 describe('favorite Blog', () => {
   const listBlogs = [
     {
@@ -60,6 +113,58 @@ describe('favorite Blog', () => {
   test('blog with many likes', () => {
     const result = listHelper.favoriteBlog(listBlogs)
     assert.deepStrictEqual(result, listBlogs[1])
+  })
+})
+
+describe('author with most blog', () => {
+  const listBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0
+    }
+
+  ]
+
+  test('author with many blogs', () => {
+    const result = listHelper.mostBlogs(listBlogs)
+    console.log(result)
+    assert.deepStrictEqual(result, { author: listBlogs[2].author, blogs: 3 })
   })
 })
 
--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -12,6 +12,30 @@ const favoriteBlog = (blogs) => {
   return blogs.find(blog => blog.likes === maxLikes)
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+  const authorCounts = blogs.reduce((counts, blog) => {
+    counts[blog.author] = (counts[blog.author] || 0) + 1
+    return counts
+  }, {})
+
+  const topAuthor = Object.keys(authorCounts).reduce((a, b) =>
+    authorCounts[b] > authorCounts[a] ? b : a
+  )
+
+  return {
+    author: topAuthor,
+    blogs: authorCounts[topAuthor]
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+  const mostLikesBlog = blogs.find(blog => blog.likes === maxLikes)
+  return { author: mostLikesBlog.author, likes: mostLikesBlog.likes }
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
